1bda3e7ca7efbb204a8482ad3e395d94964afd13
Fernando Rodriguez <94305744+frodbej@users.noreply.github.com>
2022-11-12 16:52:53 +0100

    Add seq-utils.py

---
+++ seq-utils.py
@@ -0,0 +1,53 @@
+
+# some useful functions to deal with some basics operations on biological sequences
+
+def reverse_complement(seq):
+    """Take as input a sequence and return the reverse complement sequence."""
+
+    comp_base = {
+        'A':'T',
+        'T':'A',
+        'C':'G',
+        'G':'C'
+    }
+
+    return ''.join([comp_base[nt] for nt in seq[::-1]])
+
+
+def GCCont(seq):
+    """Return GC content of a sequence."""
+
+    count, l = 0, len(seq)
+    for i in range(0,l):
+        if seq[i] in "GC":
+            count += 1
+    return count/l
+
+
+def maxHomopolymerLength(seq):
+    """Return the length of the longest homopolymer in a sequence."""
+
+    lMaxHom, lCurHom, l = 1, 1, len(seq)
+    for i in range(1,l):
+        if seq[i] == seq[i-1]:
+            lCurHom += 1
+        else:
+            if lCurHom > lMaxHom:
+                lMaxHom = lCurHom
+            lCurHom = 1
+    return lMaxHom
+
+
+def loadFasta(filename):
+    """Open fasta file and return two lists: one for headers and one for sequences"""
+
+    with open(filename, 'r') as f:
+        # split at headers
+        data = f.read().split(">")
+        data.pop(0)
+        headers, sequences = [], []
+        for sequence in data:
+            lines = sequence.split('\n')
+            headers.append(lines.pop(0))
+            sequences.append(''.join(lines))
+    return (headers, sequences)
